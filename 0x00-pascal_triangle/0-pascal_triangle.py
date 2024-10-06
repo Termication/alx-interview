87985f79bb644d29fe7d70fab9415a1a08283d99
innocent <terminalkarabo@gmail.com>
2024-10-06 18:31:36 +0000

    pascal triangle algo change

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
     """
     Returns a list of lists representing Pascal's triangle of size n
@@ -6,14 +7,19 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # First row of Pascal's triangle
+    triangle = [0] * n
 
-    for i in range(1, n):
-        row = [1]  # Every row starts with 1
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            # Each element is the sum of the two elements directly above it
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # Every row ends with 1
-        triangle.append(row)
+            if j > 0 and j < len(new_row):
+                a = triangle[i - 1][j]
+                b = triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        triangle[i] = new_row
 
     return triangle
