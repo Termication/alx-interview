d45df751317275adaa31f59e1044765f2c1e954c
innocent <terminalkarabo@gmail.com>
2024-10-13 09:35:49 +0000

    Lockboxes bug fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,24 @@
 def canUnlockAll(boxes):
     '''Determines if all boxes can be unlocked.'''
-    n = len(boxes)  # Total number of boxes
-    unlocked = [False] * n  # Track which boxes are unlocked
-    unlocked[0] = True  # The first box is always unlocked
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    keys = [0]  # Start with the keys from the first box
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    # Iterate through the keys and unlock boxes
-    while keys:
-        current_key = keys.pop(0)
-        for key in boxes[current_key]:
-            if key < n and not unlocked[key]:  # Check if the key is valid and the box is not already unlocked
-                unlocked[key] = True
-                keys.append(key)  # Add the keys from the newly unlocked box
-
-    return all(unlocked)  # Check if all boxes are unlocked
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
