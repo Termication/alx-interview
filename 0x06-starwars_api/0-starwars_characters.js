f7db065d2103d0477070e48ca4e5ba6db74f7f7d
Innocent <terminalkarabo@gmail.com>
2024-11-14 21:34:46 +0000

    Script to fetch and display the names of characters in a Star Wars film.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,71 +1,25 @@
 #!/usr/bin/node
-/**
- * Script to print all characters of a given Star Wars movie.
- *
- * Usage:
- * - The first positional argument is the Movie ID (e.g., 3 = "Return of the Jedi").
- * - Fetches character names in the same order as listed in the "characters" array from the /films/ endpoint.
- */
-
 const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
-// Check if a Movie ID is provided
 if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const url = `${API_URL}/films/${movieId}/`;
-
-  // Fetch the film details using the Movie ID
-  request(url, (err, res, body) => {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
-      console.error(err);
-      return;
+      console.log(err);
     }
-
-    // Check for valid response status
-    if (res.statusCode !== 200) {
-      console.error(`Failed to fetch film. Status code: ${res.statusCode}`);
-      return;
-    }
-
-    let charactersURL;
-    try {
-      charactersURL = JSON.parse(body).characters;
-    } catch (parseErr) {
-      console.error('Failed to parse JSON response:', parseErr);
-      return;
-    }
-
-    // Function to fetch character names and display them in order
-    const fetchCharacterName = (url) => {
-      return new Promise((resolve, reject) => {
-        request(url, (charErr, charRes, charBody) => {
-          if (charErr) {
-            reject(charErr);
-          } else if (charRes.statusCode !== 200) {
-            reject(new Error(`Failed to fetch character. Status code: ${charRes.statusCode}`));
-          } else {
-            try {
-              const name = JSON.parse(charBody).name;
-              resolve(name);
-            } catch (nameParseErr) {
-              reject(nameParseErr);
-            }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
           }
+          resolve(JSON.parse(charactersReqBody).name);
         });
-      });
-    };
+      }));
 
-    // Create a list of promises to fetch all character names
-    const charactersPromises = charactersURL.map(fetchCharacterName);
-
-    // Resolve all promises and print the character names in order
-    Promise.all(charactersPromises)
-      .then((names) => {
-        names.forEach((name) => console.log(name));
-      })
-      .catch((allErr) => {
-        console.error(allErr);
-      });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
