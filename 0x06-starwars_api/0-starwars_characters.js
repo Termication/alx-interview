42d26c91c15a270581ce8bdff56df33ae18d40fc
Innocent <terminalkarabo@gmail.com>
2024-11-14 21:40:00 +0000

    Script to fetch and display the names of characters in a Star Wars film.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,54 @@
 #!/usr/bin/node
+
+/**
+ * Script to print all character names of a given Star Wars movie.
+ * The first argument passed is the Movie ID (e.g., 3 = "Return of the Jedi").
+ * Fetches and prints character names in the same order as listed in the "characters" array.
+ * Uses the Star Wars API.
+ */
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+// Get the Movie ID from the command line argument
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+/**
+ * Recursive function to fetch and print character names one by one.
+ * @param {string[]} characterList - List of character URLs.
+ * @param {number} index - Current index in the character list.
+ */
+function sendRequest (characterList, index) {
+  // Base case: if all characters have been processed, return.
+  if (characterList.length === index) {
+    return;
+  }
+
+  // Send a request to fetch the character details.
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      // Parse the character name and print it.
+      console.log(JSON.parse(body).name);
+
+      // Recursively fetch the next character.
+      sendRequest(characterList, index + 1);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
   });
 }
+
+/**
+ * Fetch movie details using the given Movie ID.
+ */
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // Extract the list of character URLs from the movie response.
+    const characterList = JSON.parse(body).characters;
+
+    // Start fetching character names from the list.
+    sendRequest(characterList, 0);
+  }
+});
