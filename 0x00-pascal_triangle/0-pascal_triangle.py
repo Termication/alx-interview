a729c232563115ac3d78b843774ccdb3cf0a4a1e
Innocent <terminalkarabo@gmail.com>
2025-01-15 19:29:09 +0000

    You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,19 +7,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [0] * n
+    triangle = [[1]]
 
-    for i in range(n):
-        new_row = [0] * (i+1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
 
-        for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = triangle[i - 1][j]
-                b = triangle[i - 1][j - 1]
-                new_row[j] = a + b
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])
 
-        triangle[i] = new_row
+        new_row.append(1)
+        triangle.append(new_row)
 
     return triangle
